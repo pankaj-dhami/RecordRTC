9c1e0f1eac47c1225778889b90c1a2a10c792bf7
pankaj-dhami <reachpankajdhami@gmail.com>
2015-10-08 06:37:57 -0400

    aa

--- RecordRTC/Global.asax.cs
+++ RecordRTC/Global.asax.cs
@@ -15,6 +15,7 @@ namespace RecordRTC
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
